9ac91109d0572f72e3d5bb74dcb21b6fdb8b561b
Naveen Rethinasamy <NRethinasamy@corp.encoress.com>
2024-10-04 11:22:27 +0530

    Solved challenge 3

---
+++ server.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+    "fmt"
+    "log"
+    "net/http"
+    "os"
+)
+
+func main() {
+    http.HandleFunc("/", FileHandler)
+    fmt.Println("Server started, running on port 8080")
+    log.Fatal(http.ListenAndServe(":8080", nil))
+}
+
+func FileHandler(w http.ResponseWriter, r *http.Request) {
+    data, err := os.ReadFile("file.txt")
+    if err != nil {
+        panic(err)
+    }
+    fmt.Fprintf(w, string(data))
+}
